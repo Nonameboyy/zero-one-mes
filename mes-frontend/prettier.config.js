be94b4903872d77b944093170e197075905463b4
ruan-cat <1219043956@qq.com>
2024-07-11 15:55:43 +0800

    ⚙️ config: 从其他项目内，同步迁移prettier中文格式化配置；

--- mes-frontend/prettier.config.js
+++ mes-frontend/prettier.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 /** @type {import("prettier").Config} */
 const config = {
+	plugins: ["prettier-plugin-lint-md"],
 	singleQuote: false,
 	printWidth: 120,
 	semi: true,
@@ -8,6 +9,10 @@ const config = {
 	useTabs: true,
 	tabWidth: 2,
 	endOfLine: "auto",
+	"space-around-alphabet": true,
+	"space-around-number": true,
+	"no-empty-code-lang": false,
+	"no-empty-code": false,
 };
 
 export default config;
