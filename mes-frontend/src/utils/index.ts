6bda9d185428b5e828e6fb757ad3eee4b5306071
ruan-cat <1219043956@qq.com>
2024-07-11 17:01:51 +0800

    🤔 save-file: 记录问题；

--- mes-frontend/src/utils/index.ts
+++ mes-frontend/src/utils/index.ts
@@ -1,5 +1,7 @@
 import type * as Prettify from "./Prettify";
 import * as conditions from "./conditions";
 
+// 有疑惑 批量导入和导出类型和包，是这样的语法么？ 还需要多看看别人组件库怎么实现的。
+
 export type { Prettify };
 export { conditions };
