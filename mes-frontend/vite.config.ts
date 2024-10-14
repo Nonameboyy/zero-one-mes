4dcb801541c2620522d2043960c9d91f8d367337
ruan-cat <1219043956@qq.com>
2024-10-14 23:26:47 +0800

    🐞 fix: 处理打包文件都压缩到唯一一个pnpm文件的错误；

--- mes-frontend/vite.config.ts
+++ mes-frontend/vite.config.ts
@@ -46,7 +46,18 @@ export default ({ mode }) =>
 				output: {
 					manualChunks(id) {
 						if (id.includes("node_modules")) {
-							return id.toString().split("node_modules/")[1].split("/")[0].toString();
+							const regex = /.pnpm\/(.*?)\//;
+							// const res = id.toString().split("node_modules/")[1].split("/")[0].toString();
+							const ids = id.toString().split("node_modules/");
+							const targetId = ids[1];
+							const chunkNames = targetId.split("/");
+							// 如果等于pnpm，说明是pnpm的包，需要取第二个
+							if (chunkNames[0] === ".pnpm") {
+								// console.log("in  chunkNames[0]", chunkNames[0]);
+								return chunkNames[1];
+							} else {
+								return chunkNames[0];
+							}
 						}
 					},
 				},
