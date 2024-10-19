d966c1d95afcb84aae3c2dfc359884dd5689b048
ruan-cat <1219043956@qq.com>
2024-10-20 00:59:43 +0800

    🦄 refactor(types): 重命名axios拓展的垫片文件。

--- mes-frontend/src/types/request.ts
+++ mes-frontend/src/types/request.ts
@@ -2,7 +2,7 @@ import type { AxiosRequestConfig } from "axios";
 import type { PartialPick } from "type-plus";
 import type { Prettify } from "utils/Prettify";
 
-import type { HttpConfig } from "../../typings/expand-axios";
+import type { HttpConfig } from "../../types/axios.shim";
 
 export type HttpConfigExtraKey = Prettify<keyof HttpConfig>;
 
