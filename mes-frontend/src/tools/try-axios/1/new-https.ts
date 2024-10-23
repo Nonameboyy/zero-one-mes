d32d2b08874fe891b7e3f587542dcf28d2b1dc60
ruan-cat <1219043956@qq.com>
2024-10-23 16:17:53 +0800

    🐳 chore: 保存。

--- mes-frontend/src/tools/try-axios/1/new-https.ts
+++ mes-frontend/src/tools/try-axios/1/new-https.ts
@@ -1,48 +1,65 @@
-import axios from "axios";
-import serverConfig from "./index";
-import qs from "qs";
-// https://zhuanlan.zhihu.com/p/509680724
+/**
+ * 尝试封装axios
+ * @see https://zhuanlan.zhihu.com/p/509680724
+ */
 
-// 创建 axios 请求实例
+import axios from "axios";
+import serverConfig from "./serverConfig";
+import qs from "qs";
+
+/** 创建 axios 请求实例 */
 const serviceAxios = axios.create({
-	baseURL: serverConfig.baseURL, // 基础请求地址
-	timeout: 10000, // 请求超时设置
-	withCredentials: false, // 跨域请求是否需要携带 cookie
+	// 基础请求地址
+	baseURL: serverConfig.baseURL,
+
+	// 请求超时设置
+	timeout: 10000,
+
+	// 跨域请求是否需要携带 cookie
+	withCredentials: false,
 });
 
-// 创建请求拦截
+/** 创建请求拦截 */
 serviceAxios.interceptors.request.use(
-	(config) => {
+	function onFulfilled(config) {
 		// 如果开启 token 认证
 		if (serverConfig.useTokenAuthorization) {
 			config.headers["Authorization"] = localStorage.getItem("token"); // 请求头携带 token
 		}
+
 		// 设置请求头
 		if (!config.headers["content-type"]) {
 			// 如果没有设置请求头
 			if (config.method === "post") {
-				config.headers["content-type"] = "application/x-www-form-urlencoded"; // post 请求
-				config.data = qs.stringify(config.data); // 序列化,比如表单数据
+				// post 请求
+				config.headers["content-type"] = "application/x-www-form-urlencoded";
+
+				// 序列化,比如表单数据
+				config.data = qs.stringify(config.data);
 			} else {
-				config.headers["content-type"] = "application/json"; // 默认类型
+				// 默认类型
+				config.headers["content-type"] = "application/json";
 			}
 		}
-		console.log("请求配置", config);
+
+		// console.log("请求配置", config);
 		return config;
 	},
-	(error) => {
+
+	function onRejected(error) {
 		Promise.reject(error);
 	},
 );
 
-// 创建响应拦截
+/** 创建响应拦截 */
 serviceAxios.interceptors.response.use(
-	(res) => {
+	function onFulfilled(res) {
 		let data = res.data;
 		// 处理自己的业务逻辑，比如判断 token 是否过期等等
 		// 代码块
 		return data;
 	},
+
 	(error) => {
 		let message = "";
 		if (error && error.response) {
--- mes-frontend/src/tools/try-axios/1/serverConfig.ts
+++ mes-frontend/src/tools/try-axios/1/serverConfig.ts
@@ -2,4 +2,5 @@ const serverConfig = {
 	baseURL: "https://smallpig.site", // 请求基础地址,可根据环境自定义
 	useTokenAuthorization: true, // 是否开启 token 认证
 };
+
 export default serverConfig;
